e29a3b8f964dbd99403fc1cd7b083da5f7dbca27
shany-shan <117829439+shany-shan@users.noreply.github.com>
2023-08-29 15:09:49 +0530

    app.py

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
+import os
 from flask import Flask,request,jsonify
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app=Flask(__name__)
-client=MongoClient ("mongodb+srv://shanpriya:SHANpriya2023@microblog.nj5juk6.mongodb.net/")
-app.db=client.api
+load_dotenv()
 
-@app.route("/users/add" , methods=["POST"])
-def add_user():
-    if request.method == "POST":
-        data=request.get_json()
-        username = data.get("username")
-        user_email = data.get("userEmail")
-        user_password = data.get("userPassword")
-        user_role = data.get("userRole")
+def create_App():
+    app=Flask(__name__)
+    client=MongoClient(os.getenv("MONGODB_URI"))
+    app.db=client.api
 
-        user_document = {
-            "username": username,
-            "user_email": user_email,
-            "user_password": user_password,
-            "user_role": user_role
-        }
-        app.db.users.insert_one(user_document)
+    @app.route("/users/add" , methods=["POST"])
+    def add_user():
+        if request.method == "POST":
+            data=request.get_json()
+            username = data.get("username")
+            user_email = data.get("userEmail")
+            user_password = data.get("userPassword")
+            user_role = data.get("userRole")
 
-    response={"message":"Added Successfully"}
-    return jsonify(response),200
+            user_document = {
+                "username": username,
+                "user_email": user_email,
+                "user_password": user_password,
+                "user_role": user_role
+            }
+            app.db.users.insert_one(user_document)
+
+        response={"message":"Added Successfully"}
+        return jsonify(response),200
 
 
-if __name__ == "__main__":
-    app.run(debug = True)
+    return app
