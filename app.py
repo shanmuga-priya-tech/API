a7a155eacfea1e2210891544333cb1fc9e8afbd3
shany-shan <117829439+shany-shan@users.noreply.github.com>
2023-08-31 13:18:09 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask,request,jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import json
+from bson import ObjectId,json_util
+import uuid
 
 load_dotenv()
 
@@ -13,47 +14,32 @@ def create_app():
 
     @app.route("/users")
     def user():
-        user = [{"user_id":entry.get("user_id"), 
-                "user_name":entry.get("username"), 
-                "user_email":entry.get("user_email"),
-                "user_password":entry.get("user_password"),
-                "user_role":entry.get("user_role")}for entry in app.db.users.find({})]
-        users=json.dumps(user,default=str)#convert objectid to str
-        return users
+        users=list(app.db.users.find())
+        return json_util.dumps(users),200
 
     @app.route("/users/<user_id>",methods=["GET"])
     def get_user(user_id):
         users=app.db.users.find_one({"user_id":user_id})
-        person=json.dumps(users,default=str)#convert objectid to str
-        return person
+        if user:
+            user["_id"]=str(user["_id"])
+            return jsonify(user),200
+        else:
+            response={"message":"Incorrect userid"}
+            return jsonify(response),400
 
     @app.route("/users/add" , methods=["POST"])
     def add_user():
-        if request.method == "POST":
             data=request.get_json()
-            user_id=data.get("user_id")
-            username = data.get("username")
-            user_email = data.get("userEmail")
-            user_password = data.get("userPassword")
-            user_role = data.get("userRole")
-
-            user_document = {
-                "user_id": user_id,
-                "username": username,
-                "user_email": user_email,
-                "user_password": user_password,
-                "user_role": user_role
-            }
-            user=app.db.users.find_one({"user_id":user_id,
-                                        "username":username,
-                                        "user_email":user_email,
-                                        "user_password":user_password})
+            user=app.db.users.find_one({"$or":[{"username":data.get("username")},
+                        {"user_email":data.get("user_email")}]})
             if user:
                 response={"message":"User with similar data Already exit"}
                 return jsonify(response),400
             else:
-                app.db.users.insert_one(user_document)
-                response={"message":"Added Successfully"}
+                user_id=str(uuid.uuid4())
+                data["user_id"]=user_id
+                app.db.users.insert_one(data)
+                response={"message":"Added Successfully","your user_id":user_id}
                 return jsonify(response),200
 
     return app
